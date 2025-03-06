974677cd88d78d9f1f49c59b7b7ed0a3fe1216c6
moradbouaiss <morad.bouaiss@gmail.com>
2025-03-06 08:52:53 +0100

    Biblio Box: attribut Box.contents et methode Box.add, testBoxAdd

--- Box.java
+++ Box.java
@@ -1,4 +1,12 @@
-public class Box {
-    public Box(){System.out.println("Box créée");
-    }
+import java.util.ArrayList;
+
+public class Box{
+
+    public Box(){System.out.println("Box créée");}
+    ArrayList<String> contents = new ArrayList<String>();
+
+    public void add(String truc) {
+        this.contents.add(truc);
+        }
+
 }
